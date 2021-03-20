6edfae951dcff2a014d531013ca893d7f4c127a5
Clay Gao <r700000@gmail.com>
2021-03-21 05:33:23 +0800

    change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -186,7 +186,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/icon.png'
+        icon: 'static/favicon-32x32.png'
       },
     },
     {
